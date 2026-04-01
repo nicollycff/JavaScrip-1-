cca70f8ee7114b3a791d1ae757dd3fb351f24769
nicollycff <nicolly.ferreira.francisco@escola.pr.gov.br>
2026-04-01 11:50:21 +0000

    Trabalho finalizado

---
+++ index.js
@@ -0,0 +1,12 @@
+const livros = require(`./listaLivros`);
+
+let maisBarato = 0; 
+
+for (let atual = 0; atual < LIVROS. length; atual++){
+    if (LIVROS[atual].preco < LIVROS[maisBarato]) {
+      maisBarato = atual  
+    }
+}
+
+console.log(`o livro mais barato custa  ${livros[maisBarato].
+    preco} e o título é ${livros[maisBarato].titulo}`)
