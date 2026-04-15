c90152e0c78bae86936b92ab10d9684c690b1d6a
nicollycff <nicolly.ferreira.francisco@escola.pr.gov.br>
2026-04-15 18:20:28 +0000

    trabalho 2

---
+++ selectionSort.js
@@ -0,0 +1,18 @@
+const livros = require (`./listaLivro`);
+const menorValor = require(`./menorValor`);
+
+
+for (let atual = 0; atual < livros.length - 1; atual++) {
+   let menor = menorValor(livros, atual)
+
+   let livroAtual = livros[atual];
+   console.log(`posição atual`, atual)
+   console.log(`livros atual`, livros[atual])
+   let livrosMenorPreco = livros[menor];
+    console.log(`livros atual`, livros[menor])
+
+   livros[atual] = livrosMenorPreco
+   livros[menor] = livrosAtual
+}
+
+console.log(livros)
